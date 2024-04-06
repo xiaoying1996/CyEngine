fd37aaf09217019fb21b9b58704825ad9938ed3c
xiaoying1996 <qwer12345678Cy>
2024-04-06 12:53:21 +0800

    add some UI Controller

--- src/OtherProjects/EngineController/header/EngineController.h
+++ src/OtherProjects/EngineController/header/EngineController.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QMessageBox>
 #include "ui_EngineController.h"
 #include "../../../Engine/MyEngine.h"
+#include "../../../Public/PublicInclude.h"
+#include "../../../Service/LogReport/LogReport.h"
 
 class EngineController : public QMainWindow
 {
@@ -10,6 +13,10 @@ class EngineController : public QMainWindow
 
 public:
     EngineController(QWidget *parent = Q_NULLPTR);
+
+public slots:
+    void on_StartBtn_clicked();
+    void slot_ThreadModeChanged(QString str);
 
 private:
     Ui::EngineControllerClass ui;
--- src/OtherProjects/EngineController/resource/EngineController.cpp
+++ src/OtherProjects/EngineController/resource/EngineController.cpp
@@ -5,5 +5,50 @@ EngineController::EngineController(QWidget *parent)
 {
     ui.setupUi(this);
     engine = MyEngine::GetInstance();
-    ErrorState err = engine->Init(1, 10);
+    ui.label_2->hide();
+    ui.label_3->hide();
+    ui.MaxThreadBox->hide();
+    ui.MinThreadBox->hide();
+    connect(ui.ThreadModePoolBox, SIGNAL(currentIndexChanged(QString)),this, SLOT(slot_ThreadModeChanged(QString)));
+}
+
+void EngineController::on_StartBtn_clicked()
+{
+    QString threadMode = ui.ThreadModePoolBox->currentText();
+    if (threadMode == QString::fromLocal8Bit("单线程"))
+    {
+        ErrorState err = engine->Init(1, 1);
+        _LOG->PrintError(err);
+        ui.StartBtn->setDisabled(true);
+    }if (threadMode == QString::fromLocal8Bit("多线程"))
+    {
+        int min = ui.MinThreadBox->currentText().toInt();
+        int max = ui.MaxThreadBox->currentText().toInt();
+        if (min > max)
+        {
+            QMessageBox::information(this, "Info", "MinThread Num More Than MaxThread Num", QMessageBox::Close);
+        }
+        else {
+            ErrorState err = engine->Init(min, max);
+            _LOG->PrintError(err);
+        }
+    }
+}
+
+void EngineController::slot_ThreadModeChanged(QString str)
+{
+    if (str == QString::fromLocal8Bit("单线程"))
+    {
+        ui.label_2->hide();
+        ui.label_3->hide();
+        ui.MaxThreadBox->hide();
+        ui.MinThreadBox->hide();
+    }
+    else if (str == QString::fromLocal8Bit("多线程"))
+    {
+        ui.label_2->show();
+        ui.label_3->show();
+        ui.MaxThreadBox->show();
+        ui.MinThreadBox->show();
+    }
 }
